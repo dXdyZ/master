91abed28342dabaf3ef710dab663edf905490bea
dXdyZ <ttttilinn85@gmail.com>
2024-02-14 23:49:11 +0300

    taco project validation datas

---
+++ taco-tools/src/main/java/org/another/tacotools/TacoToolsApplication.java
@@ -0,0 +1,13 @@
+package org.another.tacotools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoToolsApplication.class, args);
+    }
+
+}
---
+++ taco-tools/src/main/java/org/another/tacotools/controller/OrderController.java
@@ -0,0 +1,30 @@
+package org.another.tacotools.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.another.tacotools.model.TacoOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+}
