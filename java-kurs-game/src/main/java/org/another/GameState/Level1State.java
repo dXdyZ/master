2c76b33de198c8989cdba8a7a0b5ca82255d1e0f
dXdyZ <ttttilinn85@gmail.com>
2024-04-13 22:12:53 +0300

    kursach

---
+++ java-kurs-game/src/main/java/org/another/GameState/Level1State.java
@@ -0,0 +1,58 @@
+package org.another.GameState;
+
+
+
+import org.another.Main.GamePanel;
+import org.another.TileMap.TileMap;
+
+import java.awt.*;
+
+public class Level1State extends GameState {
+	
+	private TileMap tileMap;
+	
+	public Level1State(GameStateManager gsm) {
+		this.gsm = gsm;
+		init();
+	}
+	
+	public void init() {
+		
+		tileMap = new TileMap(30);
+		tileMap.loadTiles("/Tilesets/grasstileset.gif");
+		tileMap.loadMap("/Maps/level1-1.map");
+		tileMap.setPosition(0, 0);
+		
+	}
+	
+	
+	public void update() {}
+	
+	public void draw(Graphics2D g) {
+		
+		// clear screen
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		
+		// draw tilemap
+		tileMap.draw(g);
+		
+	}
+	
+	public void keyPressed(int k) {}
+	
+	public void keyReleased(int k) {}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
